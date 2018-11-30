a6020dfe98b77b2bfe8340a8592af104fe95f83f
Martin Skarzynski <marskar@gmail.com>
2018-11-30 14:36:23 -0500

    make read data script and clean up make slides script

--- make_slides.py
+++ make_slides.py
@@ -1,19 +1,52 @@
 import pypandoc
+import doctest
 
 
-def write_file(filename, contents):
+def write_file(filename: str, contents: str) -> None:
+    """Writes contents to a file named filename
+
+    Args:
+        filename: The name of the target file
+        contents: The contents of the target file
+
+    Examples:
+        >>> import tempfile
+        >>> outfile_path = tempfile.mkstemp()[1]
+        >>> write_file(outfile_path, "Test file contents")
+        >>> with open(outfile_path) as file: file.read()
+        'Test file contents'
+    """
     with open(filename, 'w') as f:
         f.write(contents)
 
 
-def make_slides(path, type):
+def make_slides(path: str = 'slides.md', type: str = 'slidy') -> str:
+    """Writes contents to a file named filename
+
+    Args:
+        path: The filepath of the target file
+        type: The HTML slideshow type, must be revealjs, slidy, or dzslides
+
+    Examples:
+        A minimal example using temporary input and output files
+        >>> import tempfile # library needed to make temp files
+        >>> infile_path = tempfile.mkstemp()[1] # tempfile 1
+        >>> write_file(infile_path, "Let's pretend this a Markdown file")
+        >>> out = tempfile.mkstemp()[1] # tempfile 2
+        >>> write_file(out, make_slides(infile_path))
+        >>> import fleep
+        >>> with open(out, "rb") as file: fleep.get(file.read(128)).extension
+        ['xml']
+    """
     if type in ('slidy', 'dzslides'):
-        return pypandoc.convert_file(path, to=type, extra_args=['-s'])
+        return pypandoc.convert_file(path, to=type, format='html', extra_args=['-s'])
     elif type == 'revealjs':
-        return pypandoc.convert_file(path, to=type,
+        return pypandoc.convert_file(path, to=type, format='html',
                                      extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js'])
     else:
-        return f'Type must be revealjs, slidy, or dzslides, not {type}'
+        raise TypeError(f'Type must be revealjs, slidy, or dzslides, not {type}.')
 
 
-write_file('slides.html', make_slides('slides.md', 'slidy'))
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
+    write_file('slides.html', make_slides())
--- read_data.py
+++ read_data.py
@@ -1,3 +1,8 @@
 import pandas as pd
-df = pd.read_csv('https://query.data.world/s/wb2m35hoycwvieh3455mrac6l5ewjs', encoding = "ISO-8859-1")
-df
+
+df = pd.read_csv('https://query.data.world/s/wb2m35hoycwvieh3455mrac6l5ewjs', encoding="ISO-8859-1")
+df.head()
+characteristics = df.iloc[:, 1:]
+breeds = df.iloc[:, :1]
+characteristics.head()
+breeds.head()
