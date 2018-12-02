760e8bb2be75a241315469c07c717b1723f56d38
kmdonahu <kmdonahu@buffalo.edu>
2018-12-02 17:34:17 -0500

    Update Graph Labels

--- DogProject1.py
+++ DogProject1.py
@@ -1,6 +1,11 @@
 
+<<<<<<< Updated upstream
 
 
 
+=======
+dogs= pd.read_csv('/Users/kaitedonahue/PycharmProjects/group-project-gitcha-head-in-the-game/AKC_Breed_Info_2.csv')
+print(dogs)
+>>>>>>> Stashed changes
 
 
--- make_plots.py
+++ make_plots.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 Ave_scatter= plt.scatter(Ave_Heights_Weights[:,0], Ave_Heights_Weights[:,1])
 
+
+
 plt.xlabel('height')
 plt.ylabel('weight')
 plt.title('Average Height and Weight of All Breeds')
@@ -12,19 +14,25 @@ plt.show()
 import matplotlib.pyplot as plt
 Ave_scatter= plt.scatter(Ave_Heights_Weights[:,0], Ave_Heights_Weights[:,1])
 
-plt.xlabel('height')
-plt.ylabel('weight')
+plt.xlabel('Average Height (inches)')
+plt.ylabel('Average Weight (lbs)')
 plt.title('Average Height and Weight of All Breeds')
 plt.show()
 
+
+
 #Make Histogram
 Ave_Height_Hist= plt.hist(Ave_Heights_Weights[:,0])
+plt.xlabel('Average Height (Inches)')
+plt.ylabel('# Dogs')
+plt.title('Average Height of All Breeds')
 plt.show()
 
+print(df.shape)
 Ave_Weight_Hist=plt.hist(Ave_Heights_Weights[:,1])
-plt.show()
-Ave_Height_Hist= plt.hist(Ave_Heights_Weights[:,0])
+plt.xlabel('Average Weight (lbs)')
+plt.ylabel('# Dogs')
+plt.title('Average Weight of All Breeds')
 plt.show()
 
-Ave_Weight_Hist=plt.hist(Ave_Heights_Weights[:,1])
-plt.show()
+
