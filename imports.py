e0c5d41919defb4b0fd8b9eb3b391a2ebda27295
micaelamccall <mica2014@comcast.net>
2018-12-02 17:08:21 -0500

    reorganize

--- imports.py
+++ imports.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import scale
 from sklearn.pipeline import make_pipeline
