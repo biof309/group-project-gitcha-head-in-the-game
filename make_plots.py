f1908ba89b69112d577d795dfc0b9d827d840355
LibbyAliberti <ealiber1@binghamton.edu>
2018-12-02 16:09:38 -0500

    Edit data to include average height and weight

--- make_plots.py
+++ make_plots.py
@@ -5,9 +5,24 @@ Ave_scatter= plt.scatter(Ave_Heights_Weights[:,0], Ave_Heights_Weights[:,1])
 plt.xlabel('height')
 plt.ylabel('weight')
 plt.title('Average Height and Weight of All Breeds')
+
+plt.show()
+
+#Make Histogram#Make Scatter Plot
+import matplotlib.pyplot as plt
+Ave_scatter= plt.scatter(Ave_Heights_Weights[:,0], Ave_Heights_Weights[:,1])
+
+plt.xlabel('height')
+plt.ylabel('weight')
+plt.title('Average Height and Weight of All Breeds')
 plt.show()
 
 #Make Histogram
+Ave_Height_Hist= plt.hist(Ave_Heights_Weights[:,0])
+plt.show()
+
+Ave_Weight_Hist=plt.hist(Ave_Heights_Weights[:,1])
+plt.show()
 Ave_Height_Hist= plt.hist(Ave_Heights_Weights[:,0])
 plt.show()
 
