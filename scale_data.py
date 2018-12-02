7006cc2eedc7314151af351163573151ad6df9d7
micaelamccall <mica2014@comcast.net>
2018-12-02 13:03:10 -0500

    read_data now includes removing NaNs added some files

--- scale_data.py
+++ scale_data.py
@@ -1,2 +1,26 @@
 scaler=StandardScaler()
 
+scaled=scaler.fit_transform(characteristics)
+np.mean(dogs.loc[:,"height_low_inches"])
+
+x=dogs.loc[:,"height_low_inches"]
+
+print(x.apply(np.mean))
+
+print(x.mean)
+
+x=dogs['height_high_inches']
+
+dogs.loc[:,'height_high_inches'].mean()
+
+dogs['height_high_inches'].astype(float)
+
+type(dogs['height_high_inches'])
+
+x_scaled=scale(x)
+
+array=characteristics.values
+type(array)
+
+
+print(array.mean)
