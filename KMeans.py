972ef1f81e0be4da6091254490337c2dea0c29af
micaelamccall <mica2014@comcast.net>
2018-12-02 15:48:24 -0500

    kmeans

--- KMeans.py
+++ KMeans.py
@@ -4,4 +4,8 @@ kmeans.fit(scaled_features)
 
 clusters=kmeans.predict(scaled_features)
 
+breedlist=list(breeds.values.T.flatten())
 
+clustered_breeds=pd.DataFrame({'clusters':clusters, 'breeds':breedlist})
+
+clustered_breeds=clustered_breeds.sort_values(by='clusters')
--- t-SNE.py
+++ t-SNE.py
@@ -7,7 +7,12 @@ y=tsne_features[:,1]
 
 breedlist=list(breeds.values.T.flatten())
 
-plt.scatter(x,y, alpha=0.5)
+
+
+plt.scatter(x,y, alpha=0.5, c=clusters)
+
+
+
 for x,y,breed in zip(x,y,breedlist):
     plt.annotate(breed, (x,y), fontsize=5)
 plt.show()
