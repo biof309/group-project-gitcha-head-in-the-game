702c92785ce8b63bd8257e8978ac5ddfe36b8abc
kmdonahu <kmdonahu@buffalo.edu>
2018-12-04 18:12:37 -0500

    deleted bad info

--- src/data/read_data.py
+++ src/data/read_data.py
@@ -15,26 +15,8 @@ dogs.shape
 
 print(dogs)
 
-import matplotlib.pyplot as plt
-from pandas.tools.plotting import table
-
-import pandas as pd
-import matplotlib.pyplot as plt
-from pandas.tools.plotting import table
-import numpy as np
 
 
-df.index = [item.strftime('%Y-%m-%d') for item in df.index] # Format date
-
-fig, ax = plt.subplots(figsize=(12, 2)) # set size frame
-ax.xaxis.set_visible(False)  # hide the x axis
-ax.yaxis.set_visible(False)  # hide the y axis
-ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
-tabla = table(ax, Dogs, loc='upper right', colWidths=[0.17]*len(df.columns))  # where df is your data frame
-tabla.auto_set_font_size(False) # Activate set fontsize manually
-tabla.set_fontsize(12) # if ++fontsize is necessary ++colWidths
-tabla.scale(1.2, 1.2) # change size table
-plt.savefig('table.png', transparent=True)
 
 
 
--- src/exploratory/make_plots.py
+++ src/exploratory/make_plots.py
@@ -3,9 +3,10 @@
 import matplotlib.pyplot as plt
 Ave_scatter= plt.scatter(Ave_Heights_Weights[:,0], Ave_Heights_Weights[:,1])
 
-plt.xlabel('Average Height (inches)')
+plt.xlabel('Average Height (Inches)')
 plt.ylabel('Average Weight (lbs)')
 plt.title('Average Height and Weight of All Breeds')
+plt.savefig(Ave_scatter.png)
 plt.show()
 
 
@@ -15,6 +16,7 @@ Ave_Height_Hist= plt.hist(Ave_Heights_Weights[:,0])
 plt.xlabel('Average Height (Inches)')
 plt.ylabel('# Dogs')
 plt.title('Average Height of All Breeds')
+plt.savefig(Ave_Height_Hist.png)
 plt.show()
 
 
@@ -22,4 +24,7 @@ Ave_Weight_Hist=plt.hist(Ave_Heights_Weights[:,1])
 plt.xlabel('Average Weight (lbs)')
 plt.ylabel('# Dogs')
 plt.title('Average Weight of All Breeds')
+plt.savefig(Ave_Weight_Hist.png)
 plt.show()
+
+
